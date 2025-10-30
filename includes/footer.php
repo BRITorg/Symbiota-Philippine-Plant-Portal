3cd9783af7dcd08fb3260faf69d1157c242c1844
Katie Pearson <katelin.d.pearson24@gmail.com>
2025-10-30 12:36:01 -0700

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -6,11 +6,11 @@
 		<a href="http://idigbio.org" target="_blank" title="iDigBio">
 			<img src="<?php echo $CLIENT_ROOT; ?>/images/layout/logo_idig.png" />
 		</a>
-		<a href="https://biokic.asu.edu" target="_blank" title="Biodiversity Knowledge Integration Center">
-			<img src="<?php echo $CLIENT_ROOT; ?>/images/layout/logo-asu-biokic.png" />
+		<a href="https://biodiversity.ku.edu" target="_blank" title="<?= $LANG['F_KU-BI'] ?>" aria-label="<?= $LANG['F_KU-BI'] ?>">
+			<img src="<?= $CLIENT_ROOT; ?>/images/layout/ku-bi_logo.png"  alt="<?= $LANG['F_KU-BI_LOGO'] ?>" />
 		</a>
 	</div>
-	<p>This project made possible by U.S. National Science Foundation Awards <a href="https://www.nsf.gov/awardsearch/showAward?AWD_ID=1754697" target="_blank">#1754697</a>.</p>
+	<p>This project made possible by U.S. National Science Foundation Award <a href="https://www.nsf.gov/awardsearch/show-award/?AWD_ID=1754697" target="_blank">#1754697</a>.</p>
 	<p>For more information about Symbiota, <a href="https://symbiota.org/docs" target="_blank" rel="noopener noreferrer">read the docs</a> or contact the <a href="https://symbiota.org/contact-the-support-hub/" target="_blank" rel="noopener noreferrer">Symbiota Support Hub</a>.</p>
 	</p>
 	<p>Powered by <a href="https://symbiota.org/" target="_blank">Symbiota</a>.</p>
